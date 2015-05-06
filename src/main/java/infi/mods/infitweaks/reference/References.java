769a9fbfd9c4023f73ba8b96a6d49cd071114594
Infinitoa <prider@gmx.de>
2015-05-06 02:27:02 +0200

    Updated Version Number

--- src/main/java/infi/mods/infitweaks/reference/References.java
+++ src/main/java/infi/mods/infitweaks/reference/References.java
@@ -4,7 +4,7 @@ public class References
 {
 	public static final String MOD_ID = "infitweaks";
 	public static final String MOD_NAME = "Infi's Tweaks";
-	public static final String MOD_VERSION = "1.7.10-0.0.1";
+	public static final String MOD_VERSION = "1.7.10-0.0.2";
 
 	public static final String CLASS_PROXY_SERVER = "infi.mods.infitweaks.proxies.ProxyServer";
 	public static final String CLASS_PROXY_CLIENT = "infi.mods.infitweaks.proxies.ProxyClient";
